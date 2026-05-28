fbd4db42f0a7e4fa3a0c707411f8f8aec6da3705
Dark <kilometro0ec@gmail.com>
2026-05-28 18:53:42 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "logica-lactea-v11";  // 👈 Incrementa este número cada vez que subas cambios
+const CACHE_NAME = "logica-lactea-v12";  // 👈 Incrementa este número cada vez que subas cambios
 const urlsToCache = [
     "/lacteos-app-/",
     "/lacteos-app-/index.html",
